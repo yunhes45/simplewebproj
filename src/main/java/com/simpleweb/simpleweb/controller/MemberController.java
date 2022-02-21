649656e64c275907b38ecab9102aa13f47452789
yunhes45 <yunhes45@gmail.com>
2022-02-22 00:37:22 +0900

    layout, css

--- src/main/java/com/simpleweb/simpleweb/controller/MemberController.java
+++ src/main/java/com/simpleweb/simpleweb/controller/MemberController.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MemberController {
 	
+	@RequestMapping("/welcomepage")
+	public String notest() {
+		
+		return "welcomepage";
+	}
+	
 	@RequestMapping("/")
 	public String test() {
 		
