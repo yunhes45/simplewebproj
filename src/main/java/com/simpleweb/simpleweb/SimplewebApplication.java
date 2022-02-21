e3cda9b8cb84121db031b7fe5129c96bcf0bbe26
yunhes45 <yunhes45@gmail.com>
2022-02-21 21:43:13 +0900

    first commit

---
+++ src/main/java/com/simpleweb/simpleweb/SimplewebApplication.java
@@ -0,0 +1,13 @@
+package com.simpleweb.simpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplewebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simpleweb/simpleweb/SimplewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleweb.simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
