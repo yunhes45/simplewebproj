3186a7b65251b66ce2cb9786f64c8becd6c667a3
yunhes45 <yunhes45@gmail.com>
2022-02-28 01:16:55 +0900

    header nav

--- src/main/java/com/simpleweb/simpleweb/controller/BoardController.java
+++ src/main/java/com/simpleweb/simpleweb/controller/BoardController.java
@@ -1,19 +1,35 @@
 package com.simpleweb.simpleweb.controller;
 
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.simpleweb.simpleweb.model.Member;
 
 @Controller
 public class BoardController {
 	
 	@RequestMapping("/mainboard")
-	public String mainboard() {
-		
+	public String mainboard(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Optional<Member> session_info = (Optional<Member>) session.getAttribute("session_info");
 		return "mainboard";
 	}
 	
 	@RequestMapping("/mypage")
-	public String mypage() {
+	public String mypage(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		Optional<Member> session_info = (Optional<Member>) session.getAttribute("session_info");
+		
+		if(session_info != null) {
+			
+		}else {
+			
+		}
 		
 		return "mypage";
 	}
